b0622b39e19a73ad98251c09ddaaa18260508351
Ubuntu <ubuntu@ip-172-31-21-31.eu-west-1.compute.internal>
2018-07-09 17:14:34 +0000

    improved credentials loading

---
+++ example_awsCredentials.js
@@ -0,0 +1,8 @@
+var credentials = {
+    "accessKeyId": "some_access_key_id",
+    "secretAccessKey": "some_secret_access_key_id",
+    "region": "some_region",
+	"endpoint": "some_endpoint_adress"
+};
+
+module.exports = credentials;
